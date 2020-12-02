aee4b9af03e75f318365c98bc219c9a96b139b52
Brian Green <briangreen@yahoo.com>
2020-12-01 23:58:26 -0500

    Add solution in Java for Day 1 part 1.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # AOC2020
 Advent of Code 2020 Solutions
+
+## Python
+- Python 3
+
+## Java
+- Java JDK 11
+- Maven Build
---
+++ java/aoc-2020/src/main/java/org/adventers/guild/aoc01_part1.java
@@ -0,0 +1,48 @@
+package org.adventers.guild;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class aoc01_part1 {
+    public static void main( String[] args )
+    {
+        Path filePath = Paths.get("data", "brian_aoc01.dat");
+        String content = "1721 979 366 299 675 1456";
+
+        try
+        {
+            content = Files.readString(filePath);
+            // System.out.println(content);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        Scanner scanner = new Scanner(content);
+        List<Integer> list = new ArrayList<>();
+        while (scanner.hasNextInt()) {
+            list.add(scanner.nextInt());
+        }
+
+        for (int x: list) {
+            // System.out.println(x);
+            int position = 1;
+            List<Integer> y_list = list.subList(position, list.size());
+            for (int y: y_list) {
+                System.out.println(x + "+" + y + "=" + (x + y));
+                if ((x + y) == 2020) {
+                    System.out.println("Found it! " + (x * y));
+                    System.exit(0);
+                }
+            }
+        }
+        // System.out.println( list );
+    }
+}
