a9843cabc3d4850269c1d6af7a2a4919675d5ef4
Brian Green <briangreen@yahoo.com>
2020-12-14 00:10:39 -0500

    Part 2 that works for the examples but does not appear to work on the data set

--- aoc13_part2.py
+++ aoc13_part2.py
@@ -9,7 +9,7 @@ import os
 
 class Aoc13:
     def __init__(self):
-        test_data_time = int("939")
+        # test_data_time = int("939")
         # test_data_sched = "7,13,x,x,59,x,31,19".split(',')  # 1068781
         # test_data_sched = "17,x,13,19".split(',')  # 3417
         # test_data_sched = "67,7,59,61".strip().split(',')  # 754018
@@ -22,10 +22,10 @@ class Aoc13:
             data_set_time = int(data_file.readline())
             data_set_sched = data_file.readline().strip().split(',')
 
-        self.data_time = test_data_time
-        self.data_sched = test_data_sched
-        # self.data_time = data_set_time
-        # self.data_sched = data_set_sched
+        # self.data_time = test_data_time
+        # self.data_sched = test_data_sched
+        self.data_time = data_set_time
+        self.data_sched = data_set_sched
 
     def run_it(self):
         found_it = False
@@ -53,6 +53,15 @@ class Aoc13:
         print(current_state)        
         t = 0
         while found_it is False:
+            t += buses[0]
+            for i in buses[1:]:
+
+                found_it = True
+                # print((t - (i - offets[i])) % i)
+                if (t - (i - offets[i])) % i != 0:
+                    found_it = False
+                    break
+            """
             for i in buses:
                 if time % i == 0:
                     current_state[i] = time
@@ -77,9 +86,9 @@ class Aoc13:
                                 break
                         if found_it:
                             print(f"time={current_state[first_bus] - buses[0]}")
-            time += 1
-            if time % 1000000 == 0:
-                print(f"{time:,}")
+            """
+        print(t)
+            
 
 if __name__ == "__main__":
     solve_it = Aoc13()
