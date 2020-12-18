197e80bdfd09c0a7c2c47f8114aad5ef325545bb
Brian Green <briangreen@yahoo.com>
2020-12-18 07:44:35 -0500

    Solution to day 17 part 2 in python 3

---
+++ aoc17_part2.py
@@ -0,0 +1,78 @@
+#
+# Advent of Code 2020 - Day 17 Part 2
+# 17 Dec 2020 Brian Green
+#
+# Problem:
+# Starting with your given initial configuration, simulate six cycles in a 4-dimensional space.
+# How many cubes are left in the active state after the sixth cycle?
+#
+
+
+class Aoc17:
+    def __init__(self):
+        self.old_space = []
+        self.new_space = []
+        self.check_space = []
+        test_data = [".#.",
+                     "..#",
+                     "###"]
+
+        data_set = ["..#..#..",
+                    "#.#...#.",
+                    "..#.....",
+                    "##....##",
+                    "#..#.###",
+                    ".#..#...",
+                    "###..#..",
+                    "....#..#"]
+
+        x = y = z = w = 0
+        # space = test_data
+        space = data_set
+
+        for row in space:
+            x = 0
+            for cube in row:
+                if cube == '#':
+                    self.old_space.append((w, z, y, x))
+                x += 1
+            y += 1
+
+    def activate_cubes(self, space, check_sector=True):
+        for w, z, y, x in space:
+            cube_on = False
+            total = 0
+            if (w, z, y, x) in self.old_space:
+                cube_on = True
+                total = -1
+            for ww in range(w-1, w+2):
+                for zz in range(z-1, z+2):
+                    for yy in range(y-1, y+2):
+                        for xx in range(x-1, x+2):
+                            if check_sector and (ww, zz, yy, xx) not in self.check_space and (ww, zz, yy, xx) not in self.old_space:
+                                self.check_space.append((ww, zz, yy, xx))
+                            if (ww, zz, yy, xx) in self.old_space:
+                                total += 1
+            if cube_on:
+                if total == 2 or total == 3:
+                    self.new_space.append((w, z, y, x))
+            else:
+                if total == 3:
+                    self.new_space.append((w, z, y, x))
+
+    def run_it(self):
+        for i in range(6):
+            self.activate_cubes(self.old_space)
+            # print(self.new_space)
+            # print(self.check_space)
+            self.activate_cubes(self.check_space, check_sector=False)
+            # print(self.new_space)
+            print(len(self.new_space))
+            self.old_space = self.new_space.copy()
+            self.new_space = []
+            self.check_space = []
+
+
+if __name__ == "__main__":
+    solve_it = Aoc17()
+    solve_it.run_it()
