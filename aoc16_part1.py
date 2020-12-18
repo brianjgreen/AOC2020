a432289f9b109126961eb5245061ab6f2975fb9d
Brian Green <briangreen@yahoo.com>
2020-12-18 11:15:07 -0500

    Solution to day 16 puzzle part 1 in python 3

---
+++ aoc16_part1.py
@@ -0,0 +1,62 @@
+#
+# Advent of Code 2020 - Day 16 Part 1
+# 16 Dec 2020 Brian Green
+#
+# Problem:
+#
+#
+import os
+import re
+
+
+class Aoc16:
+    def __init__(self):
+        test_rules = ["class: 1-3 or 5-7",
+                      "row: 6-11 or 33-44", "seat: 13-40 or 45-50"]
+        test_tickets = [(7, 3, 47), (40, 4, 50), (55, 2, 20), (38, 6, 12)]
+
+        my_ticket = [71, 127, 181, 179, 113, 109, 79, 151, 97,
+                     107, 53, 193, 73, 83, 191, 101, 89, 149, 103, 197]
+        file_name = "data" + os.sep + "brian_aoc16_tickets.dat"
+        with open(file_name) as data_file:
+            tickets = [x.strip().split(',') for x in data_file.readlines()]
+        data_set_tickets = []
+        for i in tickets:
+            data_set_tickets.append(tuple(int(x) for x in i))
+        file_name = "data" + os.sep + "brian_aoc16_rules.dat"
+        with open(file_name) as data_file:
+            data_set_rules = [x.strip() for x in data_file.readlines()]
+
+        # self.data_rules = test_rules
+        # self.data_tickets = test_tickets
+        self.data_rules = data_set_rules
+        self.data_tickets = data_set_tickets
+
+    def run_it(self):
+        rules = {}
+        print(self.data_rules)
+        p = re.compile(r"([a-z\s]+): (\d+)-(\d+) or (\d+)-(\d+)")
+        for r in self.data_rules:
+            m = re.match(p, r)
+            field, lo_x, hi_x, lo_y, hi_y = m.groups()
+            rules[field] = (int(lo_x), int(hi_x), int(lo_y), int(hi_y))
+        print(rules)
+
+        print(self.data_tickets)
+        bad = []
+        for t in self.data_tickets:
+            for num in t:
+                invalid = True
+                for lo_x, hi_x, lo_y, hi_y in rules.values():
+                    if lo_x <= num <= hi_x or lo_y <= num <= hi_y:
+                        invalid = False
+                if invalid:
+                    print(f"{num} invalid!")
+                    bad.append(num)
+        print(bad)
+        print(sum(bad))
+
+
+if __name__ == "__main__":
+    solve_it = Aoc16()
+    solve_it.run_it()
