a4842428f352d749fa9d16a426674411d6266b03
Brian Green <briangreen@yahoo.com>
2020-12-18 00:03:31 -0500

    Partial solution for day 17 part1 in python 3

---
+++ aoc17_part1.py
@@ -0,0 +1,78 @@
+#
+# Advent of Code 2020 - Day 17 Part 1
+# 17 Dec 2020 Brian Green
+#
+# Problem:
+# Starting with your given initial configuration, simulate six cycles.
+# How many cubes are left in the active state after the sixth cycle?
+#
+
+
+class Aoc17:
+    def __init__(self):
+        test_data = [".#.",
+                     "..#",
+                     "###"]
+
+        data_set = ["..#..#..",
+                    "#.#...#.",
+                    "..#.....",
+                    "##....##",
+                    "#..#.###",
+                    ".#..#...",
+                    "###..#..",
+                    "....#..#"]
+
+        self.data = []
+        x = y = z = 0
+        space = test_data
+        # space = data_set
+
+        for row in space:
+            x = 0
+            for cube in row:
+                if cube == '#':
+                    self.data.append((z, y, x))
+                x += 1
+            y += 1
+
+    def activate_cubes(self, space):
+        new_space = []
+        print(min(space))
+        print(max(space))
+        min_z, min_y, min_x = min(space)
+        max_z, max_y, max_x = max(space)
+        for z in range(-5, 6):
+            for y in range(-5, 6):
+                for x in range(-5, 6):
+                    cube_on = False
+                    total = 0
+                    if (z, y, x) in space:
+                        cube_on = True
+                        total = -1
+                    for zz in range(z-1, z+2):
+                        for yy in range(y-1, y+2):
+                            for xx in range(x-1, x+2):
+                                if (zz, yy, xx) in space:
+                                    total += 1
+                    if cube_on:
+                        if total == 2 or total == 3:
+                            new_space.append((z, y, x))
+                    else:
+                        if total == 3:
+                            new_space.append((z, y, x))
+        return new_space
+
+    def run_it(self):
+        space = self.data
+        for _ in range(7):
+            space = self.activate_cubes(space)
+            print(space)
+            print(len(space))
+        print(min(space))
+        print(max(space))
+
+
+if __name__ == "__main__":
+    solve_it = Aoc17()
+    solve_it.run_it()
