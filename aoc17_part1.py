827a84721efea0dd5f844d6fdcb0b01f3fe964f2
Brian Green <briangreen@yahoo.com>
2020-12-18 07:29:52 -0500

    more efficient solution to day 17 part 1 in python 3

--- aoc17_part1.py
+++ aoc17_part1.py
@@ -10,6 +10,9 @@
 
 class Aoc17:
     def __init__(self):
+        self.old_space = []
+        self.new_space = []
+        self.check_space = []
         test_data = [".#.",
                      "..#",
                      "###"]
@@ -23,7 +26,6 @@ class Aoc17:
                     "###..#..",
                     "....#..#"]
 
-        self.data = []
         x = y = z = 0
         # space = test_data
         space = data_set
@@ -32,64 +34,42 @@ class Aoc17:
             x = 0
             for cube in row:
                 if cube == '#':
-                    self.data.append((z, y, x))
+                    self.old_space.append((z, y, x))
                 x += 1
             y += 1
 
-    def activate_cubes(self, space):
-        new_space = []
-        print(min(space))
-        print(max(space))
-        min_z, min_y, min_x = min(space)
-        max_z, max_y, max_x = max(space)
-        for z in range(-20, 20):
-            for y in range(-20, 20):
-                for x in range(-20, 20):
-                    cube_on = False
-                    total = 0
-                    if (z, y, x) in space:
-                        cube_on = True
-                        total = -1
-                    for zz in range(z-1, z+2):
-                        for yy in range(y-1, y+2):
-                            for xx in range(x-1, x+2):
-                                if (zz, yy, xx) in space:
-                                    total += 1
-                    if cube_on:
-                        if total == 2 or total == 3:
-                            new_space.append((z, y, x))
-                    else:
-                        if total == 3:
-                            new_space.append((z, y, x))
-        return new_space
+    def activate_cubes(self, space, check_sector=True):
+        for z, y, x in space:
+            cube_on = False
+            total = 0
+            if (z, y, x) in self.old_space:
+                cube_on = True
+                total = -1
+            for zz in range(z-1, z+2):
+                for yy in range(y-1, y+2):
+                    for xx in range(x-1, x+2):
+                        if check_sector and (zz, yy, xx) not in self.check_space and (zz, yy, xx) not in self.old_space:
+                            self.check_space.append((zz, yy, xx))
+                        if (zz, yy, xx) in self.old_space:
+                            total += 1
+            if cube_on:
+                if total == 2 or total == 3:
+                    self.new_space.append((z, y, x))
+            else:
+                if total == 3:
+                    self.new_space.append((z, y, x))
 
     def run_it(self):
-        space = self.data
-        for _ in range(6):
-            space = self.activate_cubes(space)
-            print(space)
-            print(len(space))
-        print(min(space))
-        print(max(space))
-        min_z, min_y, min_x = min(space)
-        max_z, max_y, max_x = max(space)
-        num_min_z = num_max_z = num_min_y = num_max_y = num_min_x = num_max_x = 0
-        for cube in space:
-            if min_z == cube[0]:
-                num_min_z += 1
-            if max_z == cube[0]:
-                num_max_z += 1
-            if min_y == cube[1]:
-                num_min_y += 1
-            if max_y == cube[1]:
-                num_max_y += 1
-            if min_x == cube[2]:
-                num_min_x += 1
-            if max_x == cube[2]:
-                num_max_x += 1
-
-        print(
-            f"{num_min_z} {num_max_z} {num_min_y} {num_max_y} {num_min_x} {num_max_x}")
+        for i in range(6):
+            self.activate_cubes(self.old_space)
+            # print(self.new_space)
+            # print(self.check_space)
+            self.activate_cubes(self.check_space, check_sector=False)
+            # print(self.new_space)
+            print(len(self.new_space))
+            self.old_space = self.new_space.copy()
+            self.new_space = []
+            self.check_space = []
 
 
 if __name__ == "__main__":
