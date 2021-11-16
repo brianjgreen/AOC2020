8d617d81f80c767d1cf183f8fb9d6012c88f1219
Brian Green <briangreen@yahoo.com>
2021-11-16 17:01:30 -0500

    AOC2015 day1 solutions parts 1 and 2

---
+++ python/aoc-2015/aoc-201501p2.py
@@ -0,0 +1,27 @@
+
+# Advent of Code 2015 - Day 1 Part 1
+# 16 Nov 2021 Brian Green
+#
+# Problem:
+#
+
+import os
+
+filename = "data" + os.sep + "brian_aoc201501.dat"
+with open(filename) as data_file:
+    data_set = data_file.readlines()
+
+# print(data_set)
+
+floor = 0
+position = 0
+for move in data_set[0]:
+    position += 1
+    if move == ")":
+        floor -= 1
+    else:
+        floor += 1
+    if floor == -1:
+        print(f"BASEMENT pos={position}")
+
+print(floor)
