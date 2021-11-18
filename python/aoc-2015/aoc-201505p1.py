7879d2c83c2489a67dce80effa4e8f78d8fa40a1
Brian Green <briangreen@yahoo.com>
2021-11-17 23:00:14 -0500

    aoc 2015 day 5 parts 1 and 2

---
+++ python/aoc-2015/aoc-201505p1.py
@@ -0,0 +1,37 @@
+
+# Advent of Code 2015 - Day 5 Part 1
+# 17 Nov 2021 Brian Green
+#
+# Problem:
+#
+
+import os
+
+filename = "data" + os.sep + "brian_aoc201505.dat"
+with open(filename) as data_file:
+    data_set = data_file.readlines()
+
+# print(data_set)
+
+nice = 0
+
+for message in data_set:
+    # at least three vowels
+    vowels = 0
+    for letter in 'aeiou':
+        vowels += message.count(letter)
+    if vowels < 3:
+        continue
+
+    # at least one letter appears twice in a row
+    doubles = [i+j for i, j in zip(message, message[1:]) if i == j]
+    if len(doubles) == 0:
+        continue
+
+    # does not contain the strings ab, cd, pq, or xy
+    if 'ab' in message or 'cd' in message or 'pq' in message or 'xy' in message:
+        continue
+
+    nice += 1
+
+print(nice)
---
+++ python/aoc-2015/aoc-201505p2.py
@@ -0,0 +1,39 @@
+
+# Advent of Code 2015 - Day 5 Part 2
+# 17 Nov 2021 Brian Green
+#
+# Problem:
+#
+
+import os
+
+filename = "data" + os.sep + "brian_aoc201505.dat"
+with open(filename) as data_file:
+    data_set = data_file.readlines()
+
+# print(data_set)
+
+nice = 0
+
+for message in data_set:
+    # a pair of any two letters that appears at least twice in the string without overlapping
+    pair = message[:2]
+    remain = message[2:].strip()
+    count = 0
+    while len(remain) > 1:
+        if pair in remain:
+            count += 1
+        pair = pair[1] + remain[0]
+        remain = remain[1:]
+
+    if count == 0:
+        continue
+
+    # at least one letter which repeats with exactly one letter between them
+    double_gap = [i+j for i, j in zip(message, message[2:]) if i == j]
+    if len(double_gap) == 0:
+        continue
+
+    nice += 1
+
+print(nice)
