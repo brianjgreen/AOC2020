8730091690031b9cc1854ebcc8ce65e78a5cdd35
Brian Green <briangreen@yahoo.com>
2021-12-01 21:47:57 -0500

    aoc 2016 day 1 parts 1 and 2

---
+++ python/aoc-2016/aoc201601p1.py
@@ -0,0 +1,46 @@
+
+# Advent of Code 2021 - Day 1 Part 1
+# 1 Dec 2021 Brian Green
+#
+# Problem:
+# How many blocks away is Easter Bunny HQ?
+#
+
+import os
+
+filename = "data" + os.sep + "brian_aoc201601.dat"
+with open(filename) as data_file:
+    data_set = data_file.readlines()
+
+directions = data_set[0].split(', ')
+
+testcase1 = ['R2', 'L3']  # 5 blocks away.
+testcase2 = ['R2', 'R2', 'R2']  # 2 blocks away.
+testcase3 = ['R5', 'L5', 'R5', 'R3']  # 12 blocks away.
+
+compass = {
+    "north": {"x": 0, "y": 1, "L": "west", "R": "east"},
+    "east": {"x": 1, "y": 0, "L": "north", "R": "south"},
+    "south": {"x": 0, "y": -1, "L": "east", "R": "west"},
+    "west": {"x": -1, "y": 0, "L": "south", "R": "north"}
+}
+
+
+def calc_distance(directions):
+    distance = 0
+    needle = 'north'
+    x = 0
+    y = 0
+    for move in directions:
+        needle = compass[needle][move[0]]
+        steps = int(move[1:])
+        x += steps * compass[needle]['x']
+        y += steps * compass[needle]['y']
+
+    return abs(x) + abs(y)
+
+
+print(calc_distance(testcase1))
+print(calc_distance(testcase2))
+print(calc_distance(testcase3))
+print(calc_distance(directions))
---
+++ python/aoc-2016/aoc201601p2.py
@@ -0,0 +1,49 @@
+
+# Advent of Code 2021 - Day 1 Part 2
+# 1 Dec 2021 Brian Green
+#
+# Problem:
+# How many blocks away is the first location you visit twice?
+#
+
+import os
+
+filename = "data" + os.sep + "brian_aoc201601.dat"
+with open(filename) as data_file:
+    data_set = data_file.readlines()
+
+directions = data_set[0].split(', ')
+
+testcase1 = ['R8', 'R4', 'R4', 'R8']  # 4 blocks away.
+
+compass = {
+    "north": {"x": 0, "y": 1, "L": "west", "R": "east"},
+    "east": {"x": 1, "y": 0, "L": "north", "R": "south"},
+    "south": {"x": 0, "y": -1, "L": "east", "R": "west"},
+    "west": {"x": -1, "y": 0, "L": "south", "R": "north"}
+}
+
+
+def calc_distance(directions):
+    distance = 0
+    needle = 'north'
+    x = 0
+    y = 0
+    locations = []
+    for move in directions:
+        needle = compass[needle][move[0]]
+        steps = int(move[1:])
+        for s in range(steps):
+            x += compass[needle]['x']
+            y += compass[needle]['y']
+            here = f"{x},{y}"
+            if here in locations:
+                return abs(x) + abs(y)
+            else:
+                locations.append(here)
+
+    return 0
+
+
+print(calc_distance(testcase1))
+print(calc_distance(directions))
