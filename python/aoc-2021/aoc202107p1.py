37f7af13cd2b23cfd182cdf7add9d68a98092391
Brian Green <briangreen@yahoo.com>
2021-12-07 14:28:59 -0500

    aoc 2021 day 7 parts 1 and 2

---
+++ python/aoc-2021/aoc202107p1.py
@@ -0,0 +1,35 @@
+
+# Advent of Code 2021 - Day 7 Part 1
+# 7 Dec 2021 Brian Green
+#
+# Problem:
+# How much fuel must they spend to align to that position?
+#
+
+import os
+from typing import MutableMapping
+
+filename = "data" + os.sep + "brian_aoc202107.dat"
+with open(filename) as data_file:
+    data_set = [pos.split(',') for pos in data_file.readlines()][0]
+
+testcase = """16,1,2,0,4,2,7,1,2,14"""
+
+# data_set = testcase.split(',')
+
+crabs = [int(pos) for pos in data_set]
+
+print(crabs)
+
+min_fuel = 10000000
+for i in range(min(crabs), max(crabs) + 1):
+    total_fuel = 0
+    for pos in crabs:
+        total_fuel += abs(pos - i)
+        if total_fuel >= min_fuel:
+            break
+
+    if total_fuel < min_fuel:
+        min_fuel = total_fuel
+
+print(min_fuel)
