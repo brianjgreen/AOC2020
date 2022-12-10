9b69a0abcbbdc09d41c3bc104798348aa7179957
Brian Green <briangreen@yahoo.com>
2022-12-10 17:00:36 -0500

    aoc 2022 day 10 solutions

---
+++ python/aoc-2022/10/part1.py
@@ -0,0 +1,38 @@
+# Advent of Code 2022 - Day 10 Part 1
+# 10 Dec 2022 Brian Green
+#
+# Problem:
+# Cathode-Ray Tube, signal strength
+
+# filename = "test.dat"
+# filename = "test2.dat"
+filename = "data.dat"
+with open(filename) as data_file:
+    data_set = [num.strip().split() for num in data_file.readlines()]
+
+# print(data_set)
+
+register_V = 1
+execute = {}
+program_counter = 1
+
+for instruction in data_set:
+    execute[program_counter] = register_V
+    program_counter += 1
+    if instruction[0] == 'addx':
+        register_V += int(instruction[1])
+        execute[program_counter] = register_V
+        program_counter += 1
+
+# print(execute)
+
+# Find the signal strength during the 20th, 60th, 100th, 140th, 180th, and 220th cycles.
+signal = 0
+cycles = [19, 59, 99, 139, 179, 219]
+for state in cycles:
+    reg = execute[state]
+    # print(f"cycle={state} V={reg}")
+    signal += (reg * (state + 1))
+    # print(signal)
+
+print(signal)
---
+++ python/aoc-2022/10/part2.py
@@ -0,0 +1,37 @@
+# Advent of Code 2022 - Day 10 Part 2
+# 10 Dec 2022 Brian Green
+#
+# Problem:
+# Cathode-Ray Tube, sprite
+
+# filename = "test.dat"
+# filename = "test2.dat"
+filename = "data.dat"
+with open(filename) as data_file:
+    data_set = [num.strip().split() for num in data_file.readlines()]
+
+# print(data_set)
+
+register_V = 1
+execute = {}
+program_counter = 1
+
+for instruction in data_set:
+    execute[program_counter] = register_V
+    program_counter += 1
+    if instruction[0] == 'addx':
+        register_V += int(instruction[1])
+        execute[program_counter] = register_V
+        program_counter += 1
+
+# print(execute)
+
+print('#', end='')
+for cycle, val in execute.items():
+    h_sync = cycle % 40
+    if h_sync >= val - 1 and h_sync <= val + 1:
+        print("#", end='')
+    else:
+        print('.', end='')
+    if h_sync == 39:
+        print()
