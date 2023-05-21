5cc46d804535381f800106d48ebe25996e2d9f53
Brian Green <briangreen@yahoo.com>
2023-05-20 22:45:56 -0400

    aoc 2016 day 6 parts 1 and 2

---
+++ python/aoc-2016/aoc201606p1.py
@@ -0,0 +1,22 @@
+
+# Advent of Code 2016 - Day 6 Parts 1 and 2
+# 20 May 2023 Brian Green
+#
+# Problem:
+# Day 6: Signals and Noise
+# What is the error-corrected version of the message being sent?
+#
+
+from collections import Counter
+
+# filename = "test06.dat"
+filename = "data06.dat"
+with open(filename) as data_file:
+    data_set = [num.strip() for num in data_file.readlines()]
+
+for col in range(len(data_set[0])):
+    column = Counter([row[col] for row in data_set])
+    # print(column.most_common(1)[0][0], end="")    # Part 1
+    print(column.most_common()[-1][0], end="")      # Part 2
+
+print()                   
