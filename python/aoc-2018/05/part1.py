af1838126d28c5b729c0fa16c249fcb86dc4c0ac
Brian Green <brian.green@oracle.com>
2023-10-23 09:59:20 -0400

    solution 2018 day 5

---
+++ python/aoc-2018/05/part1.py
@@ -0,0 +1,31 @@
+
+# Advent of Code 2018 - Day 5 Part 1
+# 20 Oct 2023 Brian Green
+#
+# Problem:
+# How many units remain after fully reacting the polymer you scanned?
+#
+
+# filename = "test.dat"
+filename = "data.dat"
+with open(filename) as data_file:
+    data_set = [num.strip() for num in data_file.readlines()]
+
+smallest = False
+data_set = data_set[0]
+while not smallest:
+    smallest = True
+    ptr = 0
+    new_str = ''
+    while ptr < len(data_set) - 1:
+        curr = data_set[ptr]
+        next = data_set[ptr + 1]
+        if curr.lower() == next.lower() and curr != next:
+            smallest = False
+            ptr += 2
+        else:
+            ptr += 1
+            new_str += curr
+    data_set = new_str + next
+
+print(len(data_set))
---
+++ python/aoc-2018/05/part2.py
@@ -0,0 +1,42 @@
+
+# Advent of Code 2018 - Day 5 Part 2
+# 23 Oct 2023 Brian Green
+#
+# Problem:
+# What is the length of the shortest polymer you can produce?
+#
+
+import string
+
+# filename = "test.dat"
+filename = "data.dat"
+with open(filename) as data_file:
+    data_set = [num.strip() for num in data_file.readlines()]
+
+orig_poly = data_set[0]
+short_poly = len(orig_poly)
+for element in string.ascii_lowercase:
+    print(element)
+    smallest = False
+    lower_set = orig_poly.replace(element, '')
+    data_set = lower_set.replace(element.upper(), '')
+    while not smallest:
+        smallest = True
+        ptr = 0
+        new_str = ''
+        while ptr < len(data_set) - 1:
+            curr = data_set[ptr]
+            next = data_set[ptr + 1]
+            if curr.lower() == next.lower() and curr != next:
+                smallest = False
+                ptr += 2
+            else:
+                ptr += 1
+                new_str += curr
+        data_set = new_str + next
+
+        poly_size = len(data_set)
+        if poly_size < short_poly:
+            short_poly = poly_size
+
+print(short_poly)
