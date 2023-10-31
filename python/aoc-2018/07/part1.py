27e0314ee0a785ae95558d36cd8c48268057adf0
Brian Green <brian.green@oracle.com>
2023-10-31 12:50:01 -0400

    cleanup and optimize 2018 day 7 part 1

--- python/aoc-2018/07/part1.py
+++ python/aoc-2018/07/part1.py
@@ -1,11 +1,12 @@
 
 # Advent of Code 2018 - Day 7 Part 1
-# 25 Oct 2023 Brian Green
+# 31 Oct 2023 Brian Green
 #
 # Problem:
 # What is the size of the largest area that isn't infinite?
 #
 
+import bisect
 import collections
 
 # filename = "test.dat"
@@ -19,37 +20,28 @@ for inst in data_set:
     pred = inst[5]  # Preceeding step
     next = inst[36] # Current step
     if pred not in steps:
+        # Add a new step if the preceeding step is not already in the dict
         steps[pred] = {'done': False, 'pred': []}
     if next in steps:
-        steps[next]['pred'].append(pred)
-        steps[next]['pred'] = sorted(steps[next]['pred'])
+        # Update the sorted list of preceeding steps
+        bisect.insort(steps[next]['pred'], pred)
     else:
+        # Add a new step if the next step is not already in the dict
         steps[next] = {'done': False, 'pred': [pred, ]}
 
-all_steps = collections.OrderedDict(sorted(steps.items()))
-print(all_steps)
-order = []
-done = False
-# 
-
-def find_done_steps(step):
-    if step not in order and not all_steps[step]['done']:
-        for pred in all_steps[step]['pred']:
-            if not all_steps[pred]['done']:
-                return(None)
-        all_steps[step]['done'] = True
-        return(step)
-    else:
-        return(None)
-            
+all_steps = collections.OrderedDict(sorted(steps.items()))  # Place steps in alpha order
+order = []           
 while len(order) < len(steps.keys()):
     for step in all_steps:
-        print(step)
-        found = find_done_steps(step)
-        if found is not None:
-            print(found)
-            order.append(found)
-            break
+        if step not in order and not all_steps[step]['done']:
+            blockers = 0
+            for pred in all_steps[step]['pred']:
+                if not all_steps[pred]['done']:
+                    blockers += 1
+                    break   # Not all preceeding steps done, no need to check further
+            if blockers == 0:
+                all_steps[step]['done'] = True
+                order.append(step)
+                break   # Need to check for newly unblocked steps in alpha order
 
-    print(order)
 print(''.join(order))
