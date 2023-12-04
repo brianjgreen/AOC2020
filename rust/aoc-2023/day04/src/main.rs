dbe9328126b8afdfad61ddd39b1e87a64c65ab0a
Brian Green <briangreen@yahoo.com>
2023-12-04 15:22:04 -0500

    2023 Day 4 Rust solution part 2

--- rust/aoc-2023/day04/src/main.rs
+++ rust/aoc-2023/day04/src/main.rs
@@ -3,6 +3,7 @@
 //
 // Problem 1: How many points are they worth in total?
 // Problem 2: How many total scratchcards do you end up with?
+use std::collections::HashMap;
 use std::{
     fs::File,
     io::{prelude::*, BufReader},
@@ -17,15 +18,21 @@ fn lines_from_file() -> Vec<String> {
         .collect()
 }
 
-// Tally the score of winning numbers on a card
-// Each matching number doubles the value of the prize
-fn get_score(card: &String) -> i32 {
+// Return the winning numbers and the players numbers
+fn get_winners_players(card: &String) -> (Vec<&str>, Vec<&str>) {
     // Need to optimize this code for parsing the cards in the data file.
     let card_format: Vec<&str> = card.split(":").collect();
     let numbers: Vec<&str> = card_format[1].split("|").collect();
     let winners: Vec<&str> = numbers[0].split(" ").collect();
     let my_picks: Vec<&str> = numbers[1].split(" ").collect();
+    return (winners, my_picks);
+}
+
+// Tally the score of winning numbers on a card
+// Each matching number doubles the value of the prize
+fn get_score(card: &String) -> i32 {
     let mut final_score = 0;
+    let (winners, my_picks) = get_winners_players(&card);
     for win in winners.iter() {
         for my_num in my_picks.iter() {
             if win.len() == 0 || my_num.len() == 0 {
@@ -44,6 +51,24 @@ fn get_score(card: &String) -> i32 {
     return final_score;
 }
 
+// Return the number of winning player numbers on a card
+fn get_winners(card: &String) -> i32 {
+    let mut num_of_winners = 0;
+    let (winners, my_picks) = get_winners_players(&card);
+    for win in winners.iter() {
+        for my_num in my_picks.iter() {
+            if win.len() == 0 || my_num.len() == 0 {
+                // Need to optimize the code to deal with empty strings from parser
+                continue;
+            }
+            if win == my_num {
+                num_of_winners += 1;
+            }
+        }
+    }
+    return num_of_winners;
+}
+
 // Sum all of the scores from each card
 fn score_winners(cards: &Vec<String>) -> i32 {
     let mut total_score: i32 = 0;
@@ -53,11 +78,44 @@ fn score_winners(cards: &Vec<String>) -> i32 {
     return total_score;
 }
 
+fn num_of_scratch_tickets(cards: &Vec<String>) -> i32 {
+    // let mut total_cards: i32 = 0;
+    let mut cloned_cards: HashMap<i32, i32> = HashMap::new();
+    let mut total_cards: HashMap<i32, i32> = HashMap::new();
+    let mut i: i32 = 1;
+    for scratch_card in cards {
+        // count each original card
+        total_cards.insert(i, 1);
+        // Add the number of winning payer numbers to each card
+        // This will be the number of subsequent cards to clone
+        let count = cloned_cards.entry(i).or_insert(0);
+        *count += get_winners(&scratch_card);
+        i += 1;
+    }
+    for card in 1..i {
+        // Clone a number of subsequent cards equal to the number of winning player numbers
+        let mut num_win = cloned_cards[&card];
+        while num_win != 0 {
+            // Repeat the cloning for all clones of the original card
+            let next_card = card + num_win;
+            *total_cards.get_mut(&next_card).unwrap() += total_cards[&card];
+            num_win -= 1;
+        }
+    }
+    // Need to optimize the code to sum all values of a HashMap
+    let all_values = total_cards.values();
+    let mut grand_total: i32 = 0;
+    for val in all_values {
+        grand_total += val;
+    }
+    return grand_total;
+}
+
 fn main() -> std::io::Result<()> {
     let cards = lines_from_file();
     // Part 1
     println!("Part 1 = {}", score_winners(&cards));
     // Part 2
-    // println!("Part 2 = {}", power_of_cubes(&games));
+    println!("Part 2 = {}", num_of_scratch_tickets(&cards));
     Ok(())
 }
