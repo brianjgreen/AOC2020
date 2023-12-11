0e7b470bb290ae623559354a346cc955cc2a33d8
Brian Green <briangreen@yahoo.com>
2023-12-11 16:22:56 -0500

    2023 day 9 rust solutions

--- rust/aoc-2023/day09/src/main.rs
+++ rust/aoc-2023/day09/src/main.rs
@@ -1,8 +1,8 @@
 // Advent of Code 2023 - Day 9
-// Rust solution: 10 Dec 2023 Brian Green
+// Rust solution: 11 Dec 2023 Brian Green
 //
-// Problem 1: What is the sum of these extrapolated values??
-// Problem 2: ?
+// Problem 1: What is the sum of these extrapolated values?
+// Problem 2: What is the sum of these extrapolated values?
 //use std::collections::HashMap;
 use std::{
     fs::File,
@@ -18,12 +18,14 @@ fn lines_from_file() -> Vec<String> {
         .collect()
 }
 
-fn get_next_value(sequence: &Vec<i64>) -> i64 {
-    println!("{:?}", sequence);
+// Recursive function to determine the next value of the sequence (part 1) or first value (part 2)
+fn get_next_value(sequence: &Vec<i64>, part2: bool) -> i64 {
     let mut new_seq: Vec<i64> = Vec::new();
+
+    // Check to see if the sequence is all zeros
     let mut all_zeros: bool = true;
     for i in sequence {
-        if i != 0 {
+        if i != &0 {
             all_zeros = false;
             break;
         }
@@ -31,18 +33,32 @@ fn get_next_value(sequence: &Vec<i64>) -> i64 {
     if all_zeros {
         return 0;
     }
-    
-    0
+
+    // Calcualte the next sequence using the difference between each adjacent value
+    let mut left_e = sequence[0];
+    for i in 1..sequence.len() {
+        let right_e = sequence[i];
+        new_seq.push(right_e - left_e);
+        left_e = right_e;
+    }
+    if part2 {
+        // Find the first value in the sequence
+        return sequence[0] - get_next_value(&new_seq, part2);
+    } else {
+        // Find the last value in the sequence
+        return left_e + get_next_value(&new_seq, part2);
+    }
 }
 
-fn sum_of_values(history: &Vec<String>) -> i64 {
+// Convert strings into integers, sum the last (part 1) or first (part 2) value of the sequence
+fn sum_of_values(history: &Vec<String>, part2: bool) -> i64 {
     let mut sum_vals: i64 = 0;
     for sequence in history {
         let seq: Vec<i64> = sequence
             .split(" ")
             .map(|x| x.parse::<i64>().unwrap())
             .collect();
-        sum_vals += get_next_value(&seq);
+        sum_vals += get_next_value(&seq, part2);
     }
     sum_vals
 }
@@ -50,8 +66,8 @@ fn sum_of_values(history: &Vec<String>) -> i64 {
 fn main() -> std::io::Result<()> {
     let history = lines_from_file();
     // Part 1
-    println!("Part 1 = {}", sum_of_values(&history));
+    println!("Part 1 = {}", sum_of_values(&history, false));
     // Part 2
-    // println!("Part 2 = {}", get_num_steps(&path_map, true));
+    println!("Part 2 = {}", sum_of_values(&history, true));
     Ok(())
 }
