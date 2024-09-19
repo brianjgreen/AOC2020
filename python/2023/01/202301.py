2cca88ce31d69e42ad92efbd466e30ac1d5fcb54
Brian Green <briangreen@yahoo.com>
2024-09-18 23:22:58 -0400

    2023 day 1 python part2

--- python/2023/01/202301.py
+++ python/2023/01/202301.py
@@ -14,25 +14,52 @@ def parse(puzzle_input):
     return puzzle_input.split()
 
 
+def calibration_value(value):
+    """Calibration value from first digit and last digit"""
+    found_first = False
+    first = 0
+    last = 0
+    for digit in value:
+        if digit.isdigit():
+            if not found_first:
+                first = int(digit)
+                found_first = True
+            last = int(digit)
+    return (first * 10) + last
+
+
 def part1(data):
     """Solve part 1."""
     total_calc = 0
     for value in data:
-        found_first = False
-        first = 0
-        last = 0
-        for digit in value:
-            if digit.isdigit():
-                if not found_first:
-                    first = int(digit)
-                    found_first = True
-                last = int(digit)
-        total_calc += (first * 10) + last
+        total_calc += calibration_value(value)
     return total_calc
 
 
 def part2(data):
     """Solve part 2."""
+    total_calc = 0
+    num_name = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+    }
+    for value in data:
+        # print(value)
+        for name, number in num_name.items():
+            value = value.replace(name, name + name[-1])
+        # print(value)
+        for name, number in num_name.items():
+            value = value.replace(name, number)
+        # print(value)
+        total_calc += calibration_value(value)
+    return total_calc
 
 
 def solve(puzzle_input):
