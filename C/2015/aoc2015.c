fe126fef19196cceaae2160b105e965aefe1bb43
Brian Green <briangreen@yahoo.com>
2025-08-05 18:58:08 -0400

    fix tabs vs spaces, ugh

--- C/2015/aoc2015.c
+++ C/2015/aoc2015.c
@@ -1,6 +1,6 @@
 #include "aoc2015.h"
 
 int main() {
-	day01();
-	return 0;
+    day01();
+    return 0;
 }
--- C/2015/day01.c
+++ C/2015/day01.c
@@ -5,33 +5,32 @@
 #include <string.h>
 
 int day01() {
-	const char *filename = "day01.dat";
-	char *fileContents = readFileToString(filename);
+    const char *filename = "day01.dat";
+    char *fileContents = readFileToString(filename);
 
-	if (fileContents) {
-		// printf("%s\n", fileContents);
-		int floor = 0;
-		int position = 0;
-		bool first_time = true;
-		char *move = fileContents;
-		for (unsigned long i = 0; i < strlen(fileContents);
-		     i++, move++) {
-			position++;
-			if (*move == ')') {
-				floor--;
-			} else {
-				floor++;
-			}
-			if ((floor == -1) && (first_time == true)) {
-				first_time = false;
-				printf("2015 Day 01 Part 2: %d\n", position);
-			}
-			// printf("%c", *move);
-		}
+    if (fileContents) {
+        // printf("%s\n", fileContents);
+        int floor = 0;
+        int position = 0;
+        bool first_time = true;
+        char *move = fileContents;
+        for (unsigned long i = 0; i < strlen(fileContents); i++, move++) {
+            position++;
+            if (*move == ')') {
+                floor--;
+            } else {
+                floor++;
+            }
+            if ((floor == -1) && (first_time == true)) {
+                first_time = false;
+                printf("2015 Day 01 Part 2: %d\n", position);
+            }
+            // printf("%c", *move);
+        }
 
-		printf("2015 Day 01 Part 1: %d\n", floor);
-		free(fileContents);
-	}
+        printf("2015 Day 01 Part 1: %d\n", floor);
+        free(fileContents);
+    }
 
-	return 0;
+    return 0;
 }
