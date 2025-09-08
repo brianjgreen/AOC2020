369b6f739e106a57395df1ed905a4f4e3b28fdd9
Brian Green <briangreen@yahoo.com>
2025-09-07 20:57:34 -0400

    2016 Days 2 and 3 C

--- C/2016/aoc2016.c
+++ C/2016/aoc2016.c
@@ -2,31 +2,29 @@
 
 int main() {
     day01();
-    /***
     day02();
     day03();
-    day04();
-    day05();
-    day06();
-    day07();
-    day08();
-    day09();
-    day10();
-    day11();
-    day12();
-    day13();
-    day14();
-    day15();
-    day16();
-    day17();
-    day18();
+    //day04();
+    //day05();
+    //day06();
+    //day07();
+    //day08();
+    //day09();
+    //day10();
+    //day11();
+    //day12();
+    //day13();
+    //day14();
+    //day15();
+    //day16();
+    //day17();
+    //day18();
     //day19();
-    day20();
-    day21();
+    //day20();
+    //day21();
     //day22();
-    day23();
+    //day23();
     //day24();
-    day25();
-    ***/
+    //day25();
     return 0;
 }
