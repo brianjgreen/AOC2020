2828226a5667a0a27b5e69e889ba111b256364dc
Brian Green <briangreen@yahoo.com>
2025-09-09 19:50:22 -0400

    2016 day 04 C

--- C/2016/aoc2016.c
+++ C/2016/aoc2016.c
@@ -4,7 +4,7 @@ int main() {
     day01();
     day02();
     day03();
-    //day04();
+    day04();
     //day05();
     //day06();
     //day07();
--- C/2016/aoc2016.h
+++ C/2016/aoc2016.h
@@ -1,7 +1,7 @@
 #include "day01.h"
 #include "day02.h"
 #include "day03.h"
-// #include "day04.h"
+#include "day04.h"
 // #include "day05.h"
 // #include "day06.h"
 // #include "day07.h"
