aa1be69b16f8e5ea7ab740d99ae386df28fb4cd2
Brian Green <brian.green@oracle.com>
2025-11-12 17:52:07 -0500

    C# 2016 day 2 and 2024 day 1

---
+++ Csharp/2016/Day02.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace Day02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "../../data/2016/day02.dat";
+
+            int[,] keypad = new int[,]
+            {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+            };
+
+            char[,] keypad_diamond = new char[,]
+            {
+                { '0', '0', '1', '0', '0' },
+                { '0', '2', '3', '4', '0' } ,
+                { '5', '6', '7', '8', '9' },
+                { '0', 'A', 'B', 'C', '0' },
+                { '0', '0', 'D', '0', '0' }
+            };
+
+            int x = 1;
+            int y = 1;
+            int dx = 0;
+            int dy = 2;
+
+            int max_x = 2;
+            int max_y = 2;
+            int max_dx = 4;
+            int max_dy = 4;
+
+            List<int> code = [];
+            List<char> code_diamond = [];
+            // Console.WriteLine($"0,2 {keypad_diamond[2, 0]}");
+            using (StreamReader reader = new(filePath))
+            {
+                string? line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    foreach (char c in line)
+                    {
+                        int undo_dx = 0;
+                        int undo_dy = 0;
+
+                        switch (c)
+                        {
+                            case 'U':
+                                y--;
+                                dy--;
+                                undo_dy = 1;
+                                break;
+                            case 'D':
+                                y++;
+                                dy++;
+                                undo_dy = -1;
+                                break;
+                            case 'R':
+                                x++;
+                                dx++;
+                                undo_dx = -1;
+                                break;
+                            case 'L':
+                                x--;
+                                dx--;
+                                undo_dx = 1;
+                                break;
+                            default:
+                                Console.WriteLine($"Invalid instruction {c}");
+                                break;
+                        }
+
+                        if (x < 0)
+                        {
+                            x = 0;
+                        }
+                        if (x > max_x)
+                        {
+                            x = max_x;
+                        }
+                        if (y < 0)
+                        {
+                            y = 0;
+                        }
+                        if (y > max_y)
+                        {
+                            y = max_y;
+                        }
+                        if (dx < 0)
+                        {
+                            dx = 0;
+                        }
+                        if (dx > max_dx)
+                        {
+                            dx = max_dx;
+                        }
+                        if (dy < 0)
+                        {
+                            dy = 0;
+                        }
+                        if (dy > max_dy)
+                        {
+                            dy = max_dy;
+                        }
+
+                        if (keypad_diamond[dy,dx] == '0')
+                        {
+                            dx += undo_dx;
+                            dy += undo_dy;
+                        }
+                    }
+                    code.Add(keypad[y, x]);
+                    code_diamond.Add(keypad_diamond[dy, dx]);
+                }
+            }
+
+            Console.WriteLine($"2016 Day 02 Part 1: {string.Join("", code)}");
+            Console.WriteLine($"2016 Day 02 Part 2: {string.Join("", code_diamond)}");
+        }
+    }
+}
---
+++ Csharp/2024/Day01.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+
+namespace Day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "../../data/2024/day01.dat";
+
+            int total_distamce = 0;
+            int similarity = 0;
+            List<int> left_vals = [];
+            List<int> right_vals = [];
+
+            using (StreamReader reader = new(filePath))
+            {
+                string? line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] substrings = line.Split(["   "], StringSplitOptions.None);
+
+                    left_vals.Add(int.Parse(substrings[0]));
+                    right_vals.Add(int.Parse(substrings[1]));
+                }
+            }
+
+            var sorted_left = left_vals.OrderBy(n => n).ToArray();
+            var sorted_right = right_vals.OrderBy(n => n).ToArray();
+
+            foreach (int i in Enumerable.Range(0, left_vals.Count))
+            {
+                int dist = Math.Abs(sorted_left[i] - sorted_right[i]);
+                total_distamce += dist;
+
+                int count = sorted_right.Count(n => n == sorted_left[i]);
+                similarity += sorted_left[i] * count;
+            }
+            Console.WriteLine($"2024 Day 01 Part 1: {total_distamce}");
+            Console.WriteLine($"2024 Day 01 Part 2: {similarity}");
+        }
+    }
+}
