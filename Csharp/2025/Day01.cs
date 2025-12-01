0db8da7aa0106816f47c082d3cc95d5681f6e732
Brian Green <brian.green@oracle.com>
2025-12-01 14:23:39 -0500

    2025 C# day1

---
+++ Csharp/2025/Day01.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "../../data/2025/day01.dat";
+
+            int times_at_0 = 0;
+            int ticks_when_0 = 0;
+            int dial_num = 50;
+
+            using (StreamReader reader = new(filePath))
+            {
+                string? line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    int rotations = int.Parse(line.Substring(1));
+                    int direction = 1;
+                    if (line[0] == 'L')
+                    {
+                        direction = -1;
+                    }
+                    int new_dial = (dial_num + (direction * rotations)) % 100;
+
+                    int tick_of_dial = dial_num;
+                    for (int i = 0; i < rotations; i++)
+                    {
+                        tick_of_dial += direction;
+                        if (tick_of_dial < 0)
+                        {
+                            tick_of_dial += 100;
+                        }
+                        else if (tick_of_dial > 99)
+                        {
+                            tick_of_dial -= 100;
+                        }
+
+                        if (tick_of_dial == 0)
+                        {
+                            ticks_when_0++;
+                        }
+                    }
+
+                    if (new_dial < 0)
+                    {
+                        new_dial += 100;
+                    }
+
+                    dial_num = new_dial;
+                    if (dial_num == 0)
+                    {
+                        times_at_0++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"2025 Day 01 Part 1: {times_at_0}");
+            Console.WriteLine($"2025 Day 01 Part 2: {ticks_when_0}");
+        }
+    }
+}
--- Csharp/README.md
+++ Csharp/README.md
@@ -6,10 +6,21 @@ Greatly simplified the filesystem structure. Only need *.cs file.
 dotnet run Day01.cs
 ```
 
-## .NET 9 (for reference)
+## Code Formatter
+### Install Code Formatting Tool
+```
+dotnet tool install -g csharpier
+```
+### Format Code
+```
+/Users/briangreen/.dotnet/tools/csharpier Csharp/2025/Day01.cs
+```
+
+## Archive
+### .NET 9 (for reference)
 **No longer needed. .NET 10 made this a lot simplier.**
 
-### Setup filesystem for C# solution and project files.
+#### Setup filesystem for C# solution and project files.
 | Step Description | Command |
 | ---------------- | ------- |
 | 1. Create a folder for the solution. | ```mkdir <solution_folder_name>``` |
@@ -20,20 +31,20 @@ dotnet run Day01.cs
 | 6. Repeat steps 4 and 5 to create and add any other projects. | |
 | 7. Open the current folder path (.) containing the solution using VS Code. | ```code .``` |
 
-### Execution Example
+#### Execution Example
 ```
 cd 2015
 dotnet run --project Day01
 ```
 
-### Add A New Day Example
+#### Add A New Day Example
 ```
 cd 2015
 dotnet new console -o Day02
 dotnet sln add Day02
 ```
 
-### Add a New Year Example
+#### Add a New Year Example
 ```
 mkdir 2015
 cd 2015
